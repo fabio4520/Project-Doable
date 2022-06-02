184ea897f2b3ddf6bee7e328b28638c6b8777d4b
fabio4520 <fabioleofc@gmail.com>
2022-06-02 14:00:33 -0500

    ADD: definition for STORE

--- scripts/store.js
+++ scripts/store.js
@@ -1,8 +1,33 @@
+import { getTasks } from "./services/tasks-services.js"
+
+async function fetchTasks() {
+  const tasks = await getTasks();
+  this.tasks = [...tasks];
+  this.completed = tasks
+    .filter((task) => task.completed === true)
+    .reverse();
+  this.important = tasks
+    .filter((task) => task.important === true)
+    .reverse();
+  this.pending = tasks
+    .filter((task) => task.completed === false)
+    .reverse();
+}
+
+function deleteTask(id) {
+  this.tasks = this.tasks.filter((task) => task.id != id);
+  this.completed = this.favorites.filter((task) => task.id != id);
+  this.important = this.favorites.filter((task) => task.id != id);
+  this.pending = this.favorites.filter((task) => task.id != id);
+}
+
 let STORE = {
   tasks: [],
   completed: [],
   pending: [],
-  importants: []
+  importants: [],
+  fetchTasks,
+  deleteTask
 };
 
 export default STORE;
