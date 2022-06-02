39eacfb174fe415833041e3cddbc3b2a67505310
fabio4520 <fabioleofc@gmail.com>
2022-06-02 12:14:35 -0500

    ADD: tasks services

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
+import { tokenKey } from "./scripts/config.js";
 import DOMHandler from "./scripts/dom-handler.js";
 import LoginPage from "./scripts/pages/login.js";
 import { login } from "./scripts/services/session-services.js";
+import { createTasks, deleteTasks, editTasks, getTasks, showTask } from "./scripts/services/tasks-services.js";
 import { signup } from "./scripts/services/user-services.js";
 
 DOMHandler.load(LoginPage);
-
-const credentials = {
-  email: "fabio@mail.com",
-  password: "123456"
-}
-
-// signup(credentials)
-
-// login(credentials)
