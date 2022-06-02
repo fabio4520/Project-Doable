f9a7bf67896abe38ce56c74c5b5234db7ae78ea8
fabio4520 <fabioleofc@gmail.com>
2022-06-02 12:00:00 -0500

    ADD: testing index for login and signup

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 import DOMHandler from "./scripts/dom-handler.js";
 import LoginPage from "./scripts/pages/login.js";
+import { login } from "./scripts/services/session-services.js";
+import { signup } from "./scripts/services/user-services.js";
 
 DOMHandler.load(LoginPage);
+
+const credentials = {
+  email: "fabio@mail.com",
+  password: "123456"
+}
+
+// signup(credentials)
+
+// login(credentials)
