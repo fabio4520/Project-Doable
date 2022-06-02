795530ce7e7d0874356f77e9f2db5a10c5ec364e
fabio4520 <fabioleofc@gmail.com>
2022-06-02 10:58:28 -0500

    ADD: header render component

---
+++ scripts/components/header.js
@@ -0,0 +1,13 @@
+
+export function renderHeader() {
+  return `
+  <!-- header -->
+  <header class="container is-max-desktop">
+  <a class="navbar-item" href="../../index.html">
+  <h1>📝 Doable</h1>
+  </a>
+  <button id="logout-btn" class="button is-danger is-light is-small">logout</button>
+  </header>
+  `;
+}
+
