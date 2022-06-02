d44c1182f953eb0aa6a9ec70480ef5ccdd1ce82a
fabio4520 <fabioleofc@gmail.com>
2022-06-02 15:39:06 -0500

    ADD: sort functions

--- scripts/pages/home.js
+++ scripts/pages/home.js
@@ -1,14 +1,17 @@
 import { renderHeader } from "../components/header.js";
 import STORE from "../store.js";
 import { renderCard } from "./cardTask.js";
+import DOMHandler from "../dom-handler.js"
 
 function renderHome() {
+  // console.log(STORE.tasks)
   return `
     ${renderHeader()}
 
     <div>
       <span>Sort</span>
-      <select name="" id="">
+      <select name="sort" id="my-select">
+      <option value="" disabled selected>Select your option</option>
         <option value="alphabetical">Alphabetical (a-z)</option>
         <option value="dueDate">Due date</option>
         <option value="importance">Importance</option>
@@ -28,9 +31,11 @@ function renderHome() {
     </div>
     
     <ul>
-      ${STORE.tasks
+      ${
+    STORE.tasks
         .map(task => renderCard(task))
-        .join("")}
+        .join("")
+      }
     </ul>
 
 
@@ -43,11 +48,62 @@ function renderHome() {
   `
 }
 
+function orderAlphabetical() {
+  let alphaTasks = STORE.tasks.sort((a, b) => a.title.localeCompare(b.title))
+  STORE.tasks = alphaTasks;
+}
+function orderDueDate() {
+  let dueDateTasks = [...STORE.tasks
+    .filter(t => t.due_date)
+    .sort((a, b) => a.due_date.localeCompare(b.due_date))
+                      ]
+  dueDateTasks.push(...STORE.tasks.filter(t => t.due_date == null))
+  STORE.tasks = dueDateTasks
+}
+function orderImportance() {
+  let importantTasks = [...STORE.tasks
+    .filter(t => t.important)
+    .sort((a, b) => a.title.localeCompare(b.title))
+  ]
+  importantTasks
+    .push(...(STORE.tasks.filter(t => t.important == false).sort((a, b) => a.title.localeCompare(b.title))))
+  STORE.tasks = importantTasks
+}
+function listenSort() {
+  const select = document.querySelector("#my-select");
+
+  select.addEventListener('change', (event) => {
+    switch (event.target.value) {
+      case "alphabetical":
+        orderAlphabetical();
+        console.log("alphabetical");
+        DOMHandler.reload()
+        break;
+      case "dueDate":
+        orderDueDate();
+        console.log("dueDate");
+        DOMHandler.reload()
+        break;
+      case "importance":
+        console.log("importance");
+        orderImportance();
+        DOMHandler.reload()
+        break;
+    
+      default:
+        break;
+    }
+  })
+  
+  
+  
+}
 export const HomePage = {
   toString() {
     return renderHome();
   },
   addListeners() {
+    listenSort()
     // listenCreate(),
     //   calcMainAddBtn(),
     //   listenToUnFavorite(),
