864ea7331458a1444aa6446ee8059da342e8f9a2
fabio4520 <fabioleofc@gmail.com>
2022-06-02 14:00:45 -0500

    ADD: render for home.js

--- scripts/pages/home.js
+++ scripts/pages/home.js
@@ -1,6 +1,45 @@
+import { renderHeader } from "../components/header.js";
+import STORE from "../store.js";
+import { renderCard } from "./cardTask.js";
+
 function renderHome() {
   return `
-    <h1>HomePage</h1>
+    ${renderHeader()}
+
+    <div>
+      <span>Sort</span>
+      <select name="" id="">
+        <option value="alphabetical">Alphabetical (a-z)</option>
+        <option value="dueDate">Due date</option>
+        <option value="importance">Importance</option>
+      </select>
+    </div>
+
+    <div>
+      <span>Show</span>
+      <div class="option-checkbox">
+        <input type="checkbox" id="pending" name="pending" value="pending">
+        <label for="pending">Pending</label><br>
+      </div>
+      <div class="option-checkbox">
+        <input type="checkbox" id="important" name="important" value="important">
+        <label for="important">Important</label><br>
+      </div>
+    </div>
+    
+    <ul>
+      ${STORE.tasks
+        .map(task => renderCard(task))
+        .join("")}
+    </ul>
+
+
+    <form action="" class="form">
+      <input type="text" name="title" id="title" placeholder="do the dishes...">
+      <input type="date" name="dueDate" id="dueDate">
+      <button type="submit">Add Task</button>
+    </form>
+
   `
 }
 
