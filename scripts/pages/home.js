bf605c5a67a29a2e480dba984e3fbefeb0b09c11
fabio4520 <fabioleofc@gmail.com>
2022-06-02 12:23:59 -0500

    ADD: home prototype

---
+++ scripts/pages/home.js
@@ -0,0 +1,19 @@
+function renderHome() {
+  return `
+    <h1>HomePage</h1>
+  `
+}
+
+export const HomePage = {
+  toString() {
+    return renderHome();
+  },
+  addListeners() {
+    // listenCreate(),
+    //   calcMainAddBtn(),
+    //   listenToUnFavorite(),
+    //   listenLogout(),
+    //   listenToFavorite(),
+    //   openContact();
+  },
+};
